1da214eec54ec81ba1571be24604e29ce317f0b2
Tushar Banik <b.toshar@iitg.ac.in>
2025-08-11 12:58:14 +0530

    testing packages with sample creation

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pptx import Presentation
+
 def say_hello():
     print("Hello from a function!")
 
@@ -5,14 +7,30 @@ def analyze_file(filename):
     print(f"Analyzing file: {filename}")
     return f"Analysis complete for {filename}"
 
+def create_ppt():
+    prs = Presentation()
+    title_slide_layout = prs.slide_layouts[0]
+    slide = prs.slides.add_slide(title_slide_layout)
+    title = slide.shapes.title
+    subtitle = slide.placeholders[1]
+    title.text = "Hello, World!"
+    subtitle.text = "python-pptx was here!"
+    prs.save("test.pptx")
+
 def main():
     print("🚀 Starting PowerPoint Analyzer")
-    
+
     say_hello()
-    
+
+    print("🚀 Creating PowerPoint")
+
+    create_ppt()
+
+    print("✅ Done!")
+
     result = analyze_file("sample.pptx")
     print(result)
-    
+
     print("✅ Done!")
 
 if __name__ == "__main__":
